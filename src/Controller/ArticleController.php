b800b2db1e1ab689539e6283d0dd1f16ade3f855
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-06-02 13:47:40 +0200

    CREATE : branche TP formulaire

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -14,83 +14,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/article1", name="app_article1")
+     * @Route("/article/{id}", name="app_article")
      */
-    public function index1(): Response
+    public function index(ArticleRepository $ArticleRepo, $id): Response
     {
-        // Récupérer EM
-        $em = $this->getDoctrine()->getManager();
-        // Dépréciée
 
-        // Instancier un article
-        // L'ajouter a la db via l'EM
-        $a = new Article();
-        $a->setName("Iphone14");
-        $a->setPrice(1000);
-        $em->persist($a);
-        //Ne suffit pas a créer l'article; Il sera seulement ajouté a une file;
-        $em->flush();
 
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            "article"=>$ArticleRepo->find($id)
         ]);
     }
-
-
-    /**
-     * @Route("/article2", name="app_article2")
-     */
-    public function index2(EntityManagerInterface $em): Response
-    {
-        // Ici on procède à une infection de dépendance directement dans les arguments de la fonction
-
-        // Instancier un article
-        // L'ajouter a la db via l'EM
-        $a = new Article();
-        $a->setName("Samsung GFold2");
-        $a->setPrice(2000);
-        $em->persist($a);
-        //Ne suffit pas a créer l'article; Il sera seulement ajouté a une file;
-        $em->flush();
-
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
-    }
-
-    // Les methodes de Read 
-
-    // Lire un seul article
-
-    /**
-     * @Route("/lire-article1", name="app_lire_article1")
-     */
-    public function oneArticle1(ArticleRepository $ArticleRepo): Response
-    {
-        dd($ArticleRepo->find(2));
-    }
-
-    /**
-     * @Route("/lire-article2", name="app_lire_article2")
-     */
-    public function oneArticle2(): Response
-    {
-        
-        $ArticleRepo = $this->getDoctrine()->getRepository(Article::class);
-        dd($ArticleRepo->find(1));
-    }
-
-    /**
-     * @Route("/lire-article3", name="app_lire_article3")
-     */
-    public function oneArticle3(): Response
-    {
-        $em = $this->getDoctrine()->getManager();
-        $ArticleRepo = $em->getRepository(Article::class);
-        dd($ArticleRepo->find(2));
-    }
-
-
-
-
 }
